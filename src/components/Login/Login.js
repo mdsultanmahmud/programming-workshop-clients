7c95eb721c6e93619ee1ea228595948aa065d1bf
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-10-25 18:22:55 +0600

    design login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,37 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom'
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 const Login = () => {
     return (
-        <div>
-            <h3>This is login page</h3>
-        </div>
+        <>
+            <Form className='w-50 mx-auto shadow-lg p-5 border'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-2" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+                <br />
+                <Button className='mt-2' variant="primary" type="submit">
+                    Login
+                </Button>
+                <p>Don't have an account?? Please <Link to='/register'>Register</Link></p>
+                <ButtonGroup vertical>
+                    <Button  variant="outline-danger">Login with Google</Button>
+                    <Button  variant="outline-dark">Login with Github</Button>
+                </ButtonGroup>
+            </Form>
+
+        </>
+
     );
-};
+}
 
 export default Login;
